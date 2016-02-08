29d4e088d995ac9f8f8fd4e79a92e7d453ee5c72
toli444 <toli44777@gmail.com>
2016-02-08 15:19:08 +0300

    Оптимизировано отображение объектов на карте

--- Для sc-web/Чтобы отобразить карту/geometry.drawings/src/components/openstreetmap/src/openstreetmap-component.js
+++ Для sc-web/Чтобы отобразить карту/geometry.drawings/src/components/openstreetmap/src/openstreetmap-component.js
@@ -12,106 +12,149 @@ var OpenStreetMapViewer = function(sandbox){
     
     // ---- window interface -----
 	
-	this.receiveData = function(data) {
+this.receiveData = function(data) {
 
-   var dfd = new jQuery.Deferred();
+var dfd = new jQuery.Deferred();
 		
+var objectName;
+var objectCoordinates;
 
 
-  var objName;
-  var objCoord;
+var getKeynodes = function () {
+
+	var dfd1 = new jQuery.Deferred();
+	var dfd2 = new jQuery.Deferred();
+	var dfd3 = new jQuery.Deferred();
+	var dfd4 = new jQuery.Deferred();
+	var dfd5 = new jQuery.Deferred();
+
+	SCWeb.core.Server.resolveScAddr([data], function (keynodes) {
+		objectScAddr = keynodes[data];
+		dfd1.resolve();
+	});
+
+	SCWeb.core.Server.resolveScAddr(['nrel_main_idtf'], function (keynodes) {
+		nrelMainIdtfScAddr = keynodes['nrel_main_idtf'];
+		dfd2.resolve();
+	});
+
+	SCWeb.core.Server.resolveScAddr(['lang_ru'], function (keynodes) {
+		langRuScAddr = keynodes['lang_ru'];
+		dfd3.resolve();
+	});
+
+	SCWeb.core.Server.resolveScAddr(['nrel_geographical_location'], function (keynodes) {
+		nrelGeoraphicalLocationScAddr = keynodes['nrel_geographical_location'];
+		dfd4.resolve();
+	});
+
+	SCWeb.core.Server.resolveScAddr(['nrel_WGS_84_translation'], function (keynodes) {
+		nrelWGS84translationScAddr = keynodes['nrel_WGS_84_translation'];
+		dfd5.resolve();
+	});
+
+	return $.when(dfd1, dfd2, dfd3, dfd4, dfd5)
+}
 
 
 
-		var a1 = $.Deferred(), a2 = $.Deferred();
+function getNameOfObject () {
 
-	SCWeb.core.Server.resolveScAddr([data], function(addr1) {
-		 SCWeb.core.Server.resolveScAddr(['nrel_main_idtf'], function (addr2) {
-            window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
-            addr1[data],
+	var d = new jQuery.Deferred();
+
+	window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
+		objectScAddr,
+        sc_type_arc_common | sc_type_const,
+        sc_type_link,
+        sc_type_arc_pos_const_perm,
+        nrelMainIdtfScAddr
+		]).
+	done(function(setOfNodes) {
+
+	//setOfNodes[i][2] - множество ссылок на имя объекта 
+    //Из множества ссылок ищем ту, которая с русским идентификатором
+
+		for (var i=0; i < setOfNodes.length; i++) {
+			window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_3F_A_F, [
+        	langRuScAddr,
+        	sc_type_arc_pos_const_perm,
+       	    setOfNodes[i][2]			
+			]).
+			done(function(setOfNodes2) {
+
+				//setOfNodes2[0][2] - искомая ссылка
+				//Получим её содержимое
+
+				window.sctpClient.get_link_content(setOfNodes2[0][2],'string').
+				done(function(content) {
+					objectName = content; 
+					d.resolve();
+				})
+			});
+		}
+	});
+
+	return d.promise();
+}
+
+
+function getCoordinatesOfObject ()
+{
+	var d = new jQuery.Deferred();
+
+	window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
+		objectScAddr,
+    	sc_type_arc_common | sc_type_const,
+    	sc_type_node | sc_type_const,
+    	sc_type_arc_pos_const_perm,
+    	nrelGeoraphicalLocationScAddr
+		]).
+	done(function(setOfNodes){
+		window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
+			setOfNodes[0][2],
             sc_type_arc_common | sc_type_const,
             sc_type_link,
-            sc_type_arc_pos_const_perm,
-            addr2['nrel_main_idtf']
+          	sc_type_arc_pos_const_perm,
+            nrelWGS84translationScAddr
 			]).
-		    done(function(setNodes){
-			SCWeb.core.Server.resolveScAddr(['lang_ru'], function (addr3) {
-			for (var i=0; i<setNodes.length; i++)
-			{
-			window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_3F_A_F, [
-            addr3['lang_ru'],
-            sc_type_arc_pos_const_perm,
-            setNodes[i][2]
-			]).
-		    done(function(setNodes2)
-		    {
-            window.sctpClient.get_link_content(setNodes2[0][2],'string').done(function(content){objName = content; a1.resolve(); } ).fail(function(){
-           alert('nooooo1');
-           });	
-			});
-			}
-			});
-		}).fail(function(){
-           alert('nooooo2');
-			});
-           });  
-    
-		
-			SCWeb.core.Server.resolveScAddr(['nrel_geographical_location'], function (addr2) {
-                window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
-            addr1[data],
-            sc_type_arc_common | sc_type_const,
-            sc_type_node | sc_type_const,
-            sc_type_arc_pos_const_perm,
-            addr2['nrel_geographical_location']
-			]).
-		    done(function(setNodes){
-                SCWeb.core.Server.resolveScAddr(['nrel_WGS_84_translation'], function (addr3) {
-         						    window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
-            						setNodes[0][2],
-            						sc_type_arc_common | sc_type_const,
-            						sc_type_link,
-            						sc_type_arc_pos_const_perm,
-            						addr3['nrel_WGS_84_translation']
-			]).
-		    done(function(setNodes2){
-			     window.sctpClient.get_link_content(setNodes2[0][2],'string').done(function(content2){ 
-					 objCoord = content2; a2.resolve();
-				 })
-                });
-			
-    			});
-				});
-			});
-		
-		
-			});
-	
-		
-		
-		
-		
-		$.when(a1, a2).done(function() {
-		var coordinate = objCoord.split(',');
-		localStorage.setItem('NameObj',objName);
-        localStorage.setItem('GeoObj', coordinate[0]);    //send lat to map.html
-        localStorage.setItem('GeoObj2', coordinate[1]);
-		
-		});
+		done(function(setNodes2){
+			window.sctpClient.get_link_content(setNodes2[0][2],'string').done(function(content) { 
+				objectCoordinates = content; 
+				d.resolve();
+			})
+		})
+	});
+
+	return d.promise();
+}
+
+
+getKeynodes().done(function() {
+	$.when(getNameOfObject(), getCoordinatesOfObject()).done(function() {
+
+		var def = new jQuery.Deferred();
+
+		var coordinate = objectCoordinates.split(',');
+		localStorage.setItem('NameObj',objectName);
+		localStorage.setItem('GeoObj', coordinate[0]);    //send lat to map.html
+		localStorage.setItem('GeoObj2', coordinate[1]);
+
+		def.resolve();
+                localStorage.setItem('def', def);
+});
+});
 
 		
 
-    $(this.container).empty();
-	$(this.container).append('<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=/static/components/html/map.html> </iframe>');
-	dfd.resolve();
-    return dfd.promise();
-    }
+$(this.container).empty();
+$(this.container).append('<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=/static/components/html/map.html> </iframe>');
+dfd.resolve();
+return dfd.promise();
+}
 
-    this.sandbox.eventDataAppend = $.proxy(this.receiveData, this);
-    this.sandbox.updateContent();
+this.sandbox.eventDataAppend = $.proxy(this.receiveData, this);
+this.sandbox.updateContent();
 	
-	
- 
 };
 
 
